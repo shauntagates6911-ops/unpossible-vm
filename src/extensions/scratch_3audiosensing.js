4f7a1b4d921c120d3945f0f6442aa1de6d6d6f5a
shauntagates6911-ops <shaunta.gates6911@gmail.com>
2026-08-16 15:17:48 -0400

    Add audio sensing category and blocks

---
+++ src/extensions/scratch_3audiosensing.js
@@ -0,0 +1,6 @@
+<category name="Audio Sensing 🥭" colour="#5CB1D6">
+    <block type="audiosensing_startListening" />
+    <block type="audiosensing_stopListening" />
+    <block type="audiosensing_getLoudness" />
+    <block type="audiosensing_getPitchFrequency" />
+</category>
